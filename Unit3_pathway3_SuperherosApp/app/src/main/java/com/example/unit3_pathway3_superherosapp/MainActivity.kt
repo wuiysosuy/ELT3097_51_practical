0700f554bfe38992bca01f79659ed607d36683da
wuiysosuy <huyhotboyk113@gmail.com>
2025-03-24 23:56:53 +0700

    Unit3_pathway3_SuperHeros

---
+++ Unit3_pathway3_SuperherosApp/app/src/main/java/com/example/unit3_pathway3_superherosapp/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.unit3_pathway3_superherosapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.unit3_pathway3_superherosapp.model.HeroesRepository
+import com.example.unit3_pathway3_superherosapp.ui.theme.SuperheroesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SuperheroesTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SuperheroesApp()
+                }
+            }
+        }
+    }
+
+    /**
+     * Composable that displays an app bar and a list of heroes.
+     */
+    @Composable
+    fun SuperheroesApp() {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar()
+            }
+        ) {
+
+            val heroes = HeroesRepository.heroes
+            HeroesList(heroes = heroes, contentPadding = it)
+        }
+    }
+
+    /**
+     * Composable that displays a Top Bar with an icon and text.
+     *
+     * @param modifier modifiers to set to this composable
+     */
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun TopAppBar(modifier: Modifier = Modifier) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge,
+                )
+            },
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun SuperHeroesPreview() {
+        SuperheroesTheme {
+            SuperheroesApp()
+        }
+    }
+}
---
+++ Unit3_pathway3_SuperherosApp/app/src/main/java/com/example/unit3_pathway3_superherosapp/model/Hero.kt
@@ -0,0 +1,10 @@
+package com.example.unit3_pathway3_superherosapp.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Hero(
+    @StringRes val nameRes: Int,
+    @StringRes val descriptionRes: Int,
+    @DrawableRes val imageRes: Int
+)
